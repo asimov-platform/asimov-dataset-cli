9f6fa04d203e19e6b969aef6e654543049ceac09
Samuel Sarle <samuel@sarle.com>
2025-03-28 13:49:54 +0200

    Clean up `prepare_worker_loop`

--- src/prepare.rs
+++ src/prepare.rs
@@ -3,20 +3,27 @@
 use rdf_rs::model::Statement;
 use rdf_writer::Writer;
 use std::{
-    cell::RefCell, collections::VecDeque, error::Error, fs::File, io::Write, path::PathBuf, rc::Rc,
+    cell::RefCell,
+    collections::VecDeque,
+    error::Error,
+    fs::File,
+    io::{BufReader, Write},
+    path::PathBuf,
+    rc::Rc,
+    sync::mpsc,
 };
-use tracing::{info, trace, warn};
+use tracing::info;
 
 /// Max bytes for serialized result, leaving some room for rdf_insert header.
 const MAX_FILE_SIZE: usize = 1_572_864 - 1024;
 
 /// Controls how close we want the serialized result to be to MAX_FILE_SIZE.
-const ACCEPTABLE_RATIO: f64 = 0.99;
+const ACCEPTABLE_RATIO: f64 = 0.95;
 
 pub fn prepare_datasets(files: &[String]) -> Result<(), Box<dyn Error>> {
     std::thread::scope(|s| {
-        let (batch_req_send, batch_req_recv) = std::sync::mpsc::sync_channel(10);
-        let (dataset_send, dataset_recv) = std::sync::mpsc::sync_channel(10);
+        let (batch_req_send, batch_req_recv) = mpsc::sync_channel(10);
+        let (dataset_send, dataset_recv) = mpsc::sync_channel(10);
 
         let files: Vec<String> = files.to_vec();
         let producer = files
@@ -30,14 +37,14 @@ pub fn prepare_datasets(files: &[String]) -> Result<(), Box<dyn Error>> {
                 (file, format)
             })
             .flat_map(|(file, format)| {
-                oxrdfio::RdfParser::from_format(format.unwrap())
-                    .for_reader(File::open(file).unwrap())
+                let reader = File::open(file).map(BufReader::new).unwrap();
+                oxrdfio::RdfParser::from_format(format.unwrap()).for_reader(reader)
             })
             .flatten();
 
         s.spawn(|| read_worker_loop(producer, batch_req_recv));
 
-        for _ in 0..num_cpus::get() {
+        for _ in 0..4 {
             let batch_req_send = batch_req_send.clone();
             let dataset_send = dataset_send.clone();
             s.spawn(|| prepare_worker_loop(batch_req_send, dataset_send));
@@ -49,64 +56,46 @@ pub fn prepare_datasets(files: &[String]) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-pub struct StatementBatchRequest {
-    pub amount: usize,
-    pub response_chan: oneshot::Sender<StatementBatch>,
+struct StatementBatchRequest {
+    amount: usize,
+    response_chan: oneshot::Sender<StatementBatch>,
 }
 
-pub struct StatementBatch {
-    pub quads: Vec<oxrdf::Quad>,
-    pub quad_start_index: usize,
+struct StatementBatch {
+    quads: Vec<oxrdf::Quad>,
 }
 
 #[derive(Default)]
-pub struct RDFBDataset {
-    pub data: Vec<u8>,
-    pub statement_start_index: usize,
-    pub statement_count: usize,
+struct RDFBDataset {
+    data: Vec<u8>,
+    statement_count: usize,
 }
 
-fn read_worker_loop<I>(mut producer: I, requests: std::sync::mpsc::Receiver<StatementBatchRequest>)
+fn read_worker_loop<I>(mut producer: I, requests: mpsc::Receiver<StatementBatchRequest>)
 where
     I: Iterator<Item = oxrdf::Quad>,
 {
-    let mut cur_idx: usize = 0;
     while let Ok(req) = requests.recv() {
         let mut quads: Vec<oxrdf::Quad> = Vec::with_capacity(req.amount);
 
-        for _ in 0..req.amount {
-            let Some(quad) = producer.next() else {
-                let batch = StatementBatch {
-                    quads,
-                    quad_start_index: cur_idx,
-                };
-
-                req.response_chan.send(batch).unwrap();
+        while quads.len() < req.amount {
+            if let Some(quad) = producer.next() {
+                quads.push(quad);
+            } else {
+                req.response_chan.send(StatementBatch { quads }).unwrap();
                 return;
-            };
-            quads.push(quad);
+            }
         }
 
-        let quads_len = quads.len();
-
-        let batch = StatementBatch {
-            quads,
-            quad_start_index: cur_idx,
-        };
-
-        req.response_chan.send(batch).unwrap();
-
-        cur_idx += quads_len;
+        req.response_chan.send(StatementBatch { quads }).unwrap();
     }
 }
 
 fn prepare_worker_loop(
     // producer: Arc<Mutex<std::sync::mpsc::Receiver<StatementBatch>>>,
-    batch_requests: std::sync::mpsc::SyncSender<StatementBatchRequest>,
-    sink: std::sync::mpsc::SyncSender<RDFBDataset>,
+    batch_requests: mpsc::SyncSender<StatementBatchRequest>,
+    sink: mpsc::SyncSender<RDFBDataset>,
 ) {
-    // The index for output file. Used as `prepared.{:06d}.rdfb`.
-    // let mut file_idx: usize = 1;
     // Buffer for storing statements that need to be retried
     let mut statement_buffer: VecDeque<Box<dyn Statement>> = VecDeque::new();
     // write_count is how many we're trying to serialize each iteration
@@ -123,11 +112,9 @@ fn prepare_worker_loop(
     // write_count doesn't meet ACCEPTABLE_RATIO.
     let mut best_ratio: f64 = 0.0;
 
-    let mut total_written: usize = 0;
-
     loop {
         while have_more && (statement_buffer.len() < write_count) {
-            let (batch_send, batch_rec) = oneshot::channel();
+            let (batch_send, batch_recv) = oneshot::channel();
             if batch_requests
                 .send(StatementBatchRequest {
                     amount: write_count - statement_buffer.len(),
@@ -138,7 +125,7 @@ fn prepare_worker_loop(
                 have_more = false;
                 break;
             }
-            let Ok(batch) = batch_rec.recv() else {
+            let Ok(batch) = batch_recv.recv() else {
                 have_more = false;
                 break;
             };
@@ -151,90 +138,26 @@ fn prepare_worker_loop(
             break;
         }
 
-        let data = match serialize_statements(statement_buffer.iter().take(write_count)) {
-            Ok(data) => data,
-            Err(err) if err.kind() == std::io::ErrorKind::Other => {
-                trace!(
-                    statement_count = write_count.min(statement_buffer.len()),
-                    write_count,
-                    write_count_delta,
-                    best_ratio,
-                    ?err,
-                    "failed to serialize"
-                );
+        let try_write_count = write_count.min(statement_buffer.len());
+        let ser_result = serialize_statements(statement_buffer.range(..try_write_count));
 
-                lowest_overflow = lowest_overflow.min(write_count);
-
-                // backtrack
-                write_count -= write_count_delta;
-
-                if write_count_delta == 1 {
-                    // this helps get unstuck
-                    write_count = lowest_overflow - 2;
-                } else {
-                    // the last delta was too large so pull back
-                    write_count_delta >>= 1;
-                }
-
-                if write_count_delta == 0 {
-                    write_count_delta = 1
-                };
-
-                write_count += write_count_delta;
-
-                continue;
-            }
-            Err(err) => panic!("{err}"),
+        let too_large = match ser_result {
+            Ok(ref data) => data.len() > MAX_FILE_SIZE,
+            Err(ref err) => err.kind() == std::io::ErrorKind::Other,
         };
 
-        let ratio = data.len() as f64 / MAX_FILE_SIZE as f64;
-
-        trace!(
-            data = data.len(),
-            statement_buffer = statement_buffer.len(),
-            ratio,
-            best_ratio,
-            lowest_overflow,
-            write_count,
-            write_count_delta
-        );
-
-        if 1.0 > ratio
-            && (ratio > ACCEPTABLE_RATIO
-                || ratio == best_ratio
-                || (statement_buffer.len() < write_count && !have_more))
-        {
-            let written = write_count.min(statement_buffer.len());
-            total_written += written;
-
-            sink.send(RDFBDataset {
-                data,
-                statement_start_index: 0,
-                statement_count: written,
-            })
-            .unwrap();
-
-            statement_buffer.drain(..written);
-            // reset these:
-            write_count = 1;
-            best_ratio = 0.0;
-            lowest_overflow = usize::MAX;
-
-            continue;
-        }
-
-        if ratio > 1.0 {
+        if too_large {
             // current size is larger than max
+
             if write_count == 1 {
                 let _stmt = statement_buffer.pop_front();
-                let statement_number = total_written + 1;
-                warn!(
-                    ?statement_number,
-                    "statement is too large to be published even alone"
-                );
+                // let statement_number = total_written + 1;
+                // warn!(
+                //     ?statement_number,
+                //     "statement is too large to be published even alone"
+                // );
                 continue;
             }
-
             lowest_overflow = lowest_overflow.min(write_count);
 
             // backtrack
@@ -247,8 +170,27 @@ fn prepare_worker_loop(
                 // the last delta was too large so pull back
                 write_count_delta >>= 1;
             }
-        } else {
-            // current size is smaller than max
+
+            write_count_delta = write_count_delta.max(1);
+
+            write_count += write_count_delta;
+            continue;
+        }
+
+        let data = match ser_result {
+            Ok(data) => data,
+            Err(err) => panic!("{err}"), // TODO
+        };
+
+        let ratio = data.len() as f64 / MAX_FILE_SIZE as f64;
+
+        if (ratio < ACCEPTABLE_RATIO)
+            && (ratio != best_ratio)
+            && (statement_buffer.len() > write_count || have_more)
+        {
+            // we're under the target
+            // ... and the best ratio is something else (anti-loop measure)
+            // ... and there are more statements that could be included
 
             best_ratio = best_ratio.max(ratio);
 
@@ -258,24 +200,38 @@ fn prepare_worker_loop(
             while write_count_delta >= diff {
                 write_count_delta >>= 1;
             }
+
+            write_count_delta = write_count_delta.max(1);
+
+            write_count += write_count_delta;
+
+            if (write_count + 1) >= lowest_overflow {
+                // It is possible that the final serialization of a dataset with *more* statements
+                // ends up being *smaller* after compression.
+                // If we end up here it means that the best_ratio was somewhere on N-1, N-2, ...
+                // Just accept current ratio and on next iteration this will write the file.
+            } else {
+                continue;
+            }
         }
 
-        if write_count_delta == 0 {
-            write_count_delta = 1
-        };
+        sink.send(RDFBDataset {
+            data,
+            statement_count: try_write_count,
+        })
+        .unwrap();
 
-        if (write_count + 1) >= lowest_overflow {
-            // If we end up here it means that the best_ratio was somewhere on N-1, N-2, ...
-            // Just accept current ratio and on next iteration this will write the file.
-            best_ratio = ratio;
-            continue;
-        }
+        statement_buffer.drain(..try_write_count);
 
-        write_count += write_count_delta;
+        // reset these:
+        write_count = 1;
+        best_ratio = 0.0;
+        lowest_overflow = usize::MAX;
     }
 }
 
-fn write_worker_loop(producer: std::sync::mpsc::Receiver<RDFBDataset>) {
+fn write_worker_loop(producer: mpsc::Receiver<RDFBDataset>) {
+    // The index for output file. Used as `prepared.{:06d}.rdfb`.
     let mut file_idx: usize = 1;
     let mut total_written: usize = 0;
     while let Ok(prepared) = producer.recv() {
@@ -287,7 +243,7 @@ fn write_worker_loop(producer: std::sync::mpsc::Receiver<RDFBDataset>) {
         total_written += prepared.statement_count;
         let ratio = prepared.data.len() as f64 / MAX_FILE_SIZE as f64;
         info!(
-            batch_size = prepared.data.len(),
+            batch_byte_size = prepared.data.len(),
             batch_statement_count = prepared.statement_count,
             total_statement_count = total_written,
             ratio,
